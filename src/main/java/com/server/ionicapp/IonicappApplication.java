0e277ee2114c52bcbe81081647b13e4faa29a913
kalaiselvan <kalai6095@gmail.com>
2018-07-27 02:03:01 +0530

    ionic server initial commit

---
+++ src/main/java/com/server/ionicapp/IonicappApplication.java
@@ -0,0 +1,12 @@
+package com.server.ionicapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IonicappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IonicappApplication.class, args);
+	}
+}
